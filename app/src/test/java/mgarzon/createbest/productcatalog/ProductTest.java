38cf3e90af9dec7b911ef88e55f32d1f2c047a93
pms11 <113554471+pms11@users.noreply.github.com>
2022-11-22 14:51:45 -0500

    dhfgjh

--- app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
+++ app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -2,12 +2,14 @@ package mgarzon.createbest.productcatalog;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class ProductTest {
+import static org.junit.Assert.*;
+import org.junit.Test;
 
+public class ProductTest {
     @Test
     public void checkProductName() {
         Product aProduct = new Product("1", "DELL MONITOR", 180);
-        assertEquals("Check the name of the product", "DELL MONITOR", aProduct.getProductName());
+        assertEquals("Check the  name of the product", "DELL MONITOR", aProduct.getProductName());
     }
 
     @Test
@@ -19,9 +21,11 @@ public class ProductTest {
     @Test
     public void checkProductPrice() {
         // I am making this test to fail
-        Product aProduct = new Product("1", "DELL MONITOR", 180);
-        assertEquals("Check the price of the product", "0", aProduct.getPrice());
+        Product aProduct = new Product("1", "DELL MONITOR",180);
+        assertEquals("Check the price of the product", 180 , aProduct.getPrice(),0.1);
     }
+
+}
 
 
 }
